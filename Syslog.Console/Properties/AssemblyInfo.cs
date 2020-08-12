b408b2d8ec0e5c9be91a3ee7d23d4981efd8c798
ststeiger <quandary82@hailmail.net>
2020-08-12 17:30:07 +0200

    Upgrade project

--- Syslog.Console/Properties/AssemblyInfo.cs
+++ Syslog.Console/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2020.08.12.1529")]
+[assembly: AssemblyFileVersion("2020.08.12.1529")]
--- Syslog.Server/Properties/AssemblyInfo.cs
+++ Syslog.Server/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2020.08.12.1529")]
+[assembly: AssemblyFileVersion("2020.08.12.1529")]
